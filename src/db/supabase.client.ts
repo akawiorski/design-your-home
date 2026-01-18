39ce45fc6ee5d0dd1281f3eebf2d213ea8e412aa
a.kawiorski <a.kawiorski@ocado.com>
2026-01-18 14:44:18 +0100

    feat: default user id

--- src/db/supabase.client.ts
+++ src/db/supabase.client.ts
@@ -6,3 +6,5 @@ const supabaseUrl = import.meta.env.SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.SUPABASE_KEY;
 
 export const supabaseClient = createClient<Database>(supabaseUrl, supabaseAnonKey);
+
+export const DEFAULT_USER_ID = "b2d02f87-d491-45a2-8c71-a67f386b49bf";
