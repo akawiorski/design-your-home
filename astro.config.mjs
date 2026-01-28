00610efbbb8bcd92663ea36da98276a0c86478b3
a.kawiorski <a.kawiorski@ocado.com>
2026-01-28 11:59:20 +0100

    fix: astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,22 +2,22 @@
 import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
-import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import node from "@astrojs/node";
 import cloudflare from "@astrojs/cloudflare";
 
-const isDev = import.meta.env.DEV;
-const adapter = isDev
-  ? node({
-      mode: "standalone",
-    })
-  : cloudflare();
+// const adapter = isDev
+//   ? node({
+//       mode: "standalone",
+//     })
+//   : cloudflare();
+
+const adapter = cloudflare();
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [react(), sitemap()],
+  integrations: [react()],
   server: { port: 3000 },
   vite: {
     plugins: [tailwindcss()],
