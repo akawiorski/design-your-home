d9aad06eb118ba62c9dec43089fa665db94ab6db
a.kawiorski <a.kawiorski@ocado.com>
2026-01-28 09:53:41 +0100

    feat: cloudflare integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,14 @@ import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import node from "@astrojs/node";
+import cloudflare from "@astrojs/cloudflare";
+
+const isDev = import.meta.env.DEV;
+const adapter = isDev
+  ? node({
+      mode: "standalone",
+    })
+  : cloudflare();
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,7 +22,5 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: adapter,
 });
